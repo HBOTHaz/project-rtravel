3ea55740a7298544ca11db3571271b9bef679ecf
HBOTHaz <hazandme@naver.com>
2020-09-03 20:16:21 +0900

    Updates

---
+++ precache-manifest.c59e829f60744744260faa37ba759890.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b401eaa46a1fbf2830bbd7b77c8a72ff",
+    "url": "/project-rtravel/index.html"
+  },
+  {
+    "revision": "844f98c01fdd6a12cf56",
+    "url": "/project-rtravel/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ae4adfdf104378278772",
+    "url": "/project-rtravel/static/js/2.5046e70a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-rtravel/static/js/2.5046e70a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "844f98c01fdd6a12cf56",
+    "url": "/project-rtravel/static/js/main.e09fe13b.chunk.js"
+  },
+  {
+    "revision": "da1bf15a5be1beb95661",
+    "url": "/project-rtravel/static/js/runtime-main.60313721.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/project-rtravel/static/media/logo.5d5d9eef.svg"
+  }
+]);
